74b6a0363068b31fef3498c91f62efb89d61fa43
sunil880018 <sunil880018@gmail.com>
2023-01-04 20:00:44 +0530

    user controller completed

---
+++ controllers/UserController.js
@@ -0,0 +1,176 @@
+import { User } from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
+const searchUser = async (req, res) => {
+  try {
+    const { username } = req.query;
+    const users = await User.find({
+      username: { $regex: username },
+    })
+      .limit(10)
+      .select("fullname username avatar");
+
+    return res.status(StatusCodes.OK).json({ users });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message, statusCode: error.statusCode });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      throw new BadRequestError("Please provide id");
+    }
+    const user = await User.findById(id)
+      .select("-password")
+      .populate("followers following", "-password");
+
+    if (!user) {
+      throw new NotFoundError(`user with ${id} id Not Found!`);
+    }
+    return res.status(StatusCodes.OK).json({ user });
+  } catch (error) {
+    if (error.statusCode === 404) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: error.message, statusCode: error.statusCode });
+    } else if (error.statusCode === 400)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: error.message, statusCode: error.statusCode });
+    else
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: error.message, statusCode: error.statusCode });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id, avatar, fullname, mobile, address, story, website, gender } =
+      req.body;
+    if (!fullname) {
+      throw new BadRequestError("Please provide fullname");
+    }
+
+    const user = await User.findOneAndUpdate(
+      { _id: id },
+      { avatar, fullname, mobile, address, story, website, gender }
+    );
+
+    return res.status(StatusCodes.CREATED).json({ user });
+  } catch (error) {
+    if (error.statusCode === 400)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: error.message, statusCode: error.statusCode });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message, statusCode: error.statusCode });
+  }
+};
+
+const follow = async (req, res) => {
+  try {
+    const { id, followerId } = req.params;
+    if (!id) {
+      throw new BadRequestError("Please provide user id");
+    }
+
+    const user = await User.find({
+      _id: id,
+      followers: followerId,
+    });
+
+    if (user.length > 0) {
+      throw new BadRequestError("You are already following this user.");
+    }
+
+    const newUser = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $push: {
+          followers: followerId,
+        },
+      },
+      { new: true }
+    ).populate("followers following", "-followers");
+
+    return res.status(StatusCodes.CREATED).json({ newUser });
+  } catch (error) {
+    if (error.statusCode === 400)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: error.message, statusCode: error.statusCode });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message, statusCode: error.statusCode });
+  }
+};
+
+const unfollow = async (req, res) => {
+  try {
+    const { id, followerId } = req.params;
+    if (!id) {
+      throw new BadRequestError("Please provide user id");
+    }
+    const newUser = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $pull: { followers: followerId },
+      },
+      { new: true }
+    ).populate("followers following", "-followers");
+
+    return res.status(StatusCodes.CREATED).json({ newUser });
+  } catch (error) {
+    if (error.statusCode === 400)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: error.message, statusCode: error.statusCode });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message, statusCode: error.statusCode });
+  }
+};
+
+const suggestionsUser = async (req, res) => {
+  try {
+    const newArr = [...req.user.following, req.user._id];
+
+    const num = req.query.num || 10;
+    const users = await User.aggregate([
+      { $match: { _id: { $nin: newArr } } },
+      { $sample: { size: Number(num) } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "followers",
+          foreignField: "_id",
+          as: "followers",
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "following",
+          foreignField: "_id",
+          as: "following",
+        },
+      },
+    ]).project("-password");
+
+    return res.json({
+      users,
+      result: users.length,
+    });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+export { searchUser, getUser, updateUser, follow, unfollow, suggestionsUser };
--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import { dbConnection } from "./database/db.js";
 import bodyParser from "body-parser";
 import { apiRequestLimiter } from "./middleware/apiRateLimiter.js";
 import { CONFIG } from "./config/config.js";
-
+import {
+  searchUser,
+  getUser,
+  updateUser,
+  follow,
+  unfollow,
+} from "./controllers/UserController.js";
 dbConnection();
 const app = express();
 const PORT = CONFIG.PORT;
@@ -15,6 +21,12 @@ app.use(apiRequestLimiter);
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.get("/users", searchUser);
+app.get("/user/:id", getUser);
+app.put("/user", updateUser);
+app.put("/user/follow", follow);
+app.put("/user/unfollow", unfollow);
 
 app.listen(PORT, () => {
   console.log(`server run at ${PORT}`);
